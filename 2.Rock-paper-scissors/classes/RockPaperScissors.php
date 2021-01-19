f3c2ee4492173948411247d6355b8847997eab0d
Manten Devillé <manten.d@hotmail.com>
2021-01-19 11:24:59 +0100

    Add variables and todo to class RPS

--- 2.Rock-paper-scissors/classes/RockPaperScissors.php
+++ 2.Rock-paper-scissors/classes/RockPaperScissors.php
@@ -2,9 +2,22 @@
 
 class RockPaperScissors
 {
+
+    //Variables
+    public $yourPick;
+    public $computerPick;
+    public $yourScore;
+    public $computerScore;
+
     public function run()
     {
         // This function functions as your game "engine"
         // Now it's on to you to take the steering wheel and determine how it will work
+
+        //TODO: Select player pick
+
+        //TODO: Computer picks random choice
+
+        //TODO: Display score
     }
 }
